5d5c1b2a88070b2f36e5782911a61bc2ed31d6fd
bhanu <bhanu@gmail.com>
2023-09-24 22:32:55 +0530

    Revert "First Task"
    This reverts commit 923b7963d9a96eba409b7d50496d90182876c8af.

--- Task1/src/main/java/com/example/task1/repo/ServerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.task1.repo;
-
-import com.example.task1.model.Server;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface ServerRepository extends MongoRepository<Server, String> {
-    Optional<Server> findByName(String name);
-}
